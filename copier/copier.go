bbefc095b74b2bf7babdb89e28689cbe2c9a4942
Miloslav Trmač <mitr@redhat.com>
2026-08-06 21:07:59 +0200

    Add FIXMEs for the read path
    There is no known escalation pathway for this: the code is either used with
    the canChroot code path, or for users' own files where req.Root is "/"
    anyway. The FIXMEs exist for conceptual cleanliness.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- copier/copier.go
+++ copier/copier.go
@@ -56,6 +56,12 @@ func init() {
 // replaced with all of the subdirectories under that point, and the results
 // will be concatenated.
 // The matched paths are returned in lexical order, which makes the output deterministic.
+//
+// WARNING:FIXME: This function has no concept of req.Root. When used within one,
+// if the pattern contains escaping symbolic links, extendedGlob may follow them,
+// read contents of those symbolic links (and possibly other symbolic links outside
+// of the intended root, risking revealing their contents),
+// and may return paths that evaluate to files outside of the intended root.
 func extendedGlob(pattern string) (matches []string, err error) {
 	subdirs := func(dir string) []string {
 		var subdirectories []string
@@ -1318,6 +1324,9 @@ func containsWildcards(path string) bool {
 }
 
 func copierHandlerStat(ctx context.Context, req request, pm *fileutils.PatternMatcher, idMappings *idtools.IDMappings) *response {
+	// FIXME: (At least because of extendedGlob and insecureResolvePath), this does not fully constrain the operation to req.Root.
+	// Currently known users either use chroot confinement, or only use this to access the users’ own files
+	// where a concept of req.Root is not clearly relevant.
 	errorResponse := func(fmtspec string, args ...any) *response {
 		return &response{Error: fmt.Sprintf(fmtspec, args...), Stat: statResponse{}}
 	}
@@ -1520,6 +1529,9 @@ func checkLinks(item string, req request, info os.FileInfo) (string, os.FileInfo
 }
 
 func copierHandlerGet(ctx context.Context, bulkWriter io.Writer, req request, pm *fileutils.PatternMatcher, idMappings *idtools.IDMappings) (*response, func() error, error) {
+	// FIXME: (At least because of extendedGlob and insecureResolvePath), this does not fully constrain the operation to req.Root.
+	// Currently known users either use chroot confinement, or only use this to access the users’ own files
+	// where a concept of req.Root is not clearly relevant.
 	statRequest := req
 	statRequest.Request = requestStat
 	statRequest.StatOptions = StatOptions{
@@ -2741,6 +2753,9 @@ func copierHandlerRemove(ctx context.Context, req request) *response {
 	targets := []string{req.Directory}
 	if req.RemoveOptions.AllowWildcard {
 		var err error
+		// FIXME: This might access files outside of req.Root (which is not even an input).
+		// Currently the only caller seems to be confined by chroot (and the AllowWildcard flag has no known users),
+		// so this is not an immediate risk.
 		targets, err = extendedGlob(req.Directory)
 		if err != nil {
 			return errorResponse("copier: remove: glob %q: %v", req.Directory, err)
